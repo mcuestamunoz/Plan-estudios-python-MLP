16216e54ef2f411c8e4da6b84fc80a3fc1c6e438
mcuestamunoz <mcuestamunoz@gmail.com>
2025-03-24 10:33:45 +0100

    Dia 3

--- Semana-3/d3-DNN-keras.py
+++ Semana-3/d3-DNN-keras.py
@@ -2,5 +2,15 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers # type: ignore
 
-print("TensorFlow y Keras están funcionando correctamente.")
+# Definir el modelo secuencial
+entrada = keras.Input(shape=(3,))
+x = layers.Dense(5, activation='relu')(entrada)  # Capa oculta
+salida = layers.Dense(3, activation='sigmoid')(x)  # Capa de salida
+
+modelo = keras.Model(inputs=entrada, outputs=salida)
+
+# Mostrar resumen de la red
+modelo.summary()
+
+modelo.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 
